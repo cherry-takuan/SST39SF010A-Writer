9613dab3f6a3f85b36772cbdc97dca385bfb0cf5
cherry <cherry@cherry-takuan.org>
2025-02-17 01:44:17 +0900

    Flash Baker Firmv1.0

--- src/Flash_Baker_firmware/User/main.c
+++ src/Flash_Baker_firmware/User/main.c
@@ -23,6 +23,9 @@
  */
 
 #include "debug.h"
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 
 /* Global define */
@@ -176,7 +179,7 @@ void Program_Byte(uint32_t addr ,uint16_t data)
     Data_WR(0x2AAA, 0x55);
     Data_WR(0x5555, 0xA0);
     Data_WR(addr, data);
-    Delay_Us(20);
+    Delay_Us(12);
 }
 uint16_t Read_Byte(uint32_t addr)
 {
@@ -198,7 +201,7 @@ void Erase_All()
     Data_WR(0x5555, 0xAA);
     Data_WR(0x2AAA, 0x55);
     Data_WR(0x5555, 0x10);
-    Delay_Ms(500);
+    Delay_Ms(150);
 }
 void Data_WR(uint32_t addr ,uint16_t data)
 {
@@ -209,11 +212,11 @@ void Data_WR(uint32_t addr ,uint16_t data)
     BUS_IN_SET();
     Baker_data_OE(Bit_RESET);
     ROM_WR(Bit_RESET);
-    Delay_Us(100);
+    //Delay_Us(1);
     ROM_WR(Bit_SET);
-    Delay_Us(100);
+    //Delay_Us(1);
     Baker_data_OE(Bit_SET);
-    Delay_Us(100);
+    //Delay_Us(1);
 }
 void Data_SET(uint16_t data)
 {
@@ -224,7 +227,7 @@ void Data_SET(uint16_t data)
     Addr_MSB_Latch(Bit_SET);
     Addr_LSB_Latch(Bit_SET);
     Data_Latch(Bit_SET);
-    Delay_Us(1);
+    //Delay_Us(1);
     Data_Latch(Bit_RESET);
     BUS_IN_SET();
 }
@@ -244,7 +247,7 @@ void Addr_LSB_SET(int addr_LSB)
     GPIO_Write(GPIOC,addr_LSB);
     Addr_MSB_Latch(Bit_SET);
     Addr_LSB_Latch(Bit_SET);
-    Delay_Us(1);
+    //Delay_Us(1);
     Addr_LSB_Latch(Bit_RESET);
     BUS_IN_SET();
 }
@@ -255,11 +258,63 @@ void Addr_MSB_SET(int addr_MSB)
     BUS_OUT_SET();// BUS output
     GPIO_Write(GPIOC,addr_MSB);
     Addr_MSB_Latch(Bit_SET);
-    Delay_Us(1);
+    //Delay_Us(1);
     Addr_MSB_Latch(Bit_RESET);
     BUS_IN_SET();
 }
+/*
+ * Command
+ */
 
+#define Buf_size 20
+char serial_rx[Buf_size] = "";
+
+
+int hex_str_to_int(char *str){
+  char *endptr;
+  long wk = strtol(str,&endptr,16);
+  if (str == endptr) {
+      return -1;
+  }
+  if (wk > 0xFFFFFF || wk < 0) {
+      return -1;
+  }
+  return (int)wk;
+}
+void serial_buf_clear(){
+    for(int i=0; i < Buf_size;i++){
+        serial_rx[i] = '\0';
+    }
+}
+void serialReceive(){
+    char c;
+    //文字列初期化
+    serial_buf_clear();
+    while(1){
+        while(USART_GetFlagStatus(USART1, USART_FLAG_RXNE) == RESET)
+        {/* waiting for receiving finish */}
+        c = (USART_ReceiveData(USART1));
+
+        size_t now = strlen(serial_rx);
+        if(c == '\r'){
+            continue;
+        }
+        if(c == '\n'){
+            //10文字でなければクリア，10文字なら本処理に返す
+            if(now>10) serial_buf_clear();
+            else        return;
+        }
+        if(isprint(c)){
+            if(now > 10){
+                //10文字以上であれば受信データクリアして先頭から書き込み
+                serial_buf_clear();
+                serial_rx[0] = c;
+            }else{
+                serial_rx[now] = c;
+            }
+        }
+    }
+}
 /*********************************************************************
  * @fn      main
  *
@@ -273,29 +328,52 @@ int main(void)
     SystemCoreClockUpdate();
     Delay_Init();
     USART_Printf_Init(115200);
-    printf("SystemClk:%d\r\n",SystemCoreClock);
-
     USARTx_CFG();
     GPIO_INIT();
-
-    Erase_All();
-    Program_Byte(0x0000, 0x01);
-    Program_Byte(0x0001, 0x02);
-    Program_Byte(0x0002, 0x03);
-    //Program_Byte(0x0004, 0x0A);
-    //Program_Byte(0x0005, 0xA0);
-    //Program_Byte(0x0006, 0x00);
-    //Program_Byte(0x0007, 0xCE);
-
-    printf("0x000000:%02x\n",Read_Byte(0x0000));
-    printf("0x000001:%02x\n",Read_Byte(0x0001));
-    printf("0x000002:%02x\n",Read_Byte(0x0002));
-    printf("0x000003:%02x\n",Read_Byte(0x0003));
-    printf("0x000004:%02x\n",Read_Byte(0x0004));
-    printf("0x000005:%02x\n",Read_Byte(0x0005));
-    printf("0x000006:%02x\n",Read_Byte(0x0006));
-    printf("0x000007:%02x\n",Read_Byte(0x0007));
-
-
-    while(1){}
+    Delay_Ms(1000);
+    long now_address = 0;
+    while(1)
+    {
+        serialReceive();
+        char command = serial_rx[0];
+        char Buf[14];
+        char wk[10] = "";
+        switch(command){
+          case '@':
+                  strncpy(wk,serial_rx+1,7);
+                  now_address = hex_str_to_int(wk);
+                  if(now_address == -1){
+                      printf("E\n");
+                      continue;
+                  }
+                  sprintf(Buf,"%06lX\n",now_address);
+                  printf("%s",Buf);
+                  break;
+          case 'R':
+                  printf("%02X\n",Read_Byte(now_address));
+                  now_address++;
+                  break;
+          case 'W':
+                  strncpy(wk,serial_rx+1,3);
+                  long data = hex_str_to_int(wk);
+                  if(data == -1 || data > 0xFF){
+                      printf("E\n");
+                      continue;
+                  }
+                  Program_Byte(now_address, (uint16_t)data);
+                  printf("%02X\n",Read_Byte(now_address));
+                  now_address++;
+                  break;
+          case 'D':
+                  Erase_All();
+                  printf("0\n");
+                  break;
+          case 'V':
+                  Delay_Ms(10);
+                  printf("FLASH Baker v1.0 (firm:v1.0)\n");
+                  break;
+          default:
+                  printf("E\n");
+        }
+    }
 }
